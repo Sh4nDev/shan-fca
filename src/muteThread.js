e91a84c29f270daa328bccaa7b8c50c4dab16a33
SH AN <sh4n.dev@gmail.com>
2026-08-22 14:35:17 +0600

    ShAn

---
+++ src/muteThread.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var utils = require("../utils");
+var log = require("npmlog");
+
+module.exports = function (defaultFuncs, api, ctx) {
+  return function muteThread(threadID, muteSeconds, callback) {
+    var resolveFunc = function () { };
+    var rejectFunc = function () { };
+    var returnPromise = new Promise(function (resolve, reject) {
+      resolveFunc = resolve;
+      rejectFunc = reject;
+    });
+
+    if (!callback) {
+      callback = function (err, data) {
+        if (err) return rejectFunc(err);
+
+        resolveFunc(data);
+      };
+    }
+
+    var form = {
+      thread_fbid: threadID,
+      mute_settings: muteSeconds
+    };
+
+    defaultFuncs
+      .post("https://www.facebook.com/ajax/mercury/change_mute_thread.php", ctx.jar, form)
+      .then(utils.saveCookies(ctx.jar))
+      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+      .then(function (resData) {
+        if (resData.error) throw resData;
+
+        return callback();
+      })
+      .catch(function (err) {
+        log.error("muteThread", err);
+        return callback(err);
+      });
+
+    return returnPromise;
+  };
+};
